ba5f4dc263916749607e46912e8214955ca9c0f3
bullara2 <bullara2@wwu.edu>
2023-03-07 15:00:12 -0800

    wording enc activity

--- Scripts/BasicEncryption.js
+++ Scripts/BasicEncryption.js
@@ -68,30 +68,64 @@ export default function WebTracking(props) {
                     <Text style={styles.largeTitle}>Basic Encryption</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}>Welcome to our module on the basics of encryption! First, we want to to start off with an activity on the most simple cipher, the Caesar cipher.</Text>
+                    <Text style={[styles.paragraph]}>Welcome to our module on the basics of encryption! First, we want to to start off with an activity on the most simple cipher, the Caesar cipher.</Text>
                 </View>  
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.title, {marginTop:20}]}>Interactive Activity: Caesar Cipher</Text>
+                    <Text style={[styles.paragraph]}>Imagine you are in ancient times, sending a messenger from one military camp to another.</Text>  
+                </View>      
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>You need to encrypt the message in such a way that it's gibberish if your enemies intercept the messenger, but can be decoded into English by your commanders. How can you do this?</Text>  
+                </View>   
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>First, you meet up with your commanders beforehand and agree on a special system called a Caesar Cypher. In this system, you all follow a simple rule: shift letters to the left in the alphabet when writing a letter.</Text>  
+                </View>   
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>For example, E would become B:</Text>  
+                </View>        
+                <View style={styles.contentImageContainer}>
+                    <Animated.Image source={require('../assets/infopng/caesar.png')}
+                            style={{
+                            width:width/4,
+                            height:(width/4)*(186/440)
+                        }}
+                    />
+                </View>
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>After encrypting a string of letters, or message, in this fashion you send it out into the world. After your commanders get the message, they simply perform the opposite shift to reveal the message.</Text>  
+                </View> 
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.title, {marginTop:20}]}>Interactive Activity: Caesar Cipher</Text>
                 </View>  
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}> </Text>  
-                </View>             
+                    <Text style={[styles.paragraph]}>Try typing a message of up to 15 characters, and see how it shifts.</Text>  
+                </View> 
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>What happens if you use a letter like A that doesn't have any characters "before it" in the alphabet?</Text>  
+                </View> 
+                <View style={styles.encryptionActivity}>
+                </View>
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>Feel free to keep messing around with this cipher tool until you know what's happening.</Text>  
+                </View> 
+                <View style={styles.sectionContent}>
+                    <Text style={[styles.paragraph]}>The major limitation of this cipher is its pretty easy to break. An attacker simply needs to guess or find out the shift number and methodology, and the system is exposed. Luckily, we have more advanced encryption methods available...</Text>  
+                </View> 
             </View>)}
             {section == 1 && (<View style={styles.section}>
                 <View style={styles.sectionContent}>
                     <Text style={styles.largeTitle}>Advanced Cryptography</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}>Now that we have basic encryption under our belts, we can move on to the more complex types of encryption that are commonly used.</Text>
+                    <Text style={[styles.paragraph]}>Now that we have basic encryption under our belts, we can move on to the more complex types of encryption that are commonly used.</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}>The cipher we learned in the previous section did an adequate enough job in ensuring that at a first glance, people would not be able to understand its content. However, a simple caesar cipher is not enough protection for your messages. Our attackers are not people, but the computers that run billions of operations per second. The 26! permutations needed to test for a caesar cipher is trivial.</Text>
+                    <Text style={[styles.paragraph]}>The cipher we learned in the previous section did an adequate enough job in ensuring that at a first glance, people would not be able to understand its content. However, a simple caesar cipher is not enough protection for your messages. Our attackers are not people, but the computers that run billions of operations per second. The 26! permutations needed to test for a caesar cipher is trivial.</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                <Text style = {[styles.title]}>Symmetric Cryptography</Text>
+                <Text style={[styles.title]}>Symmetric Cryptography</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}>Encrypting your message symmetrically can be one such way. Symmetric encryption will require a single key, a string of completely random letters and numbers that will be used as your cipher. This key will jumble your message in a way that can only be decrypted by the other person with the same key, thus symmetry. Already compared to the caesar cipher, depending on the length of your key, an attacker's bruteforce strategy may scare them off!</Text>
+                    <Text style={[styles.paragraph]}>Encrypting your message symmetrically can be one such way. Symmetric encryption will require a single key, a string of completely random letters and numbers that will be used as your cipher. This key will jumble your message in a way that can only be decrypted by the other person with the same key, thus symmetry. Already compared to the caesar cipher, depending on the length of your key, an attacker's bruteforce strategy may scare them off!</Text>
                 </View>
                 <View style={styles.contentImageContainer}>
                     <Animated.Image source={require('../assets/infopng/symmetric.png')}
@@ -102,7 +136,7 @@ export default function WebTracking(props) {
                     />
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph, { marginTop:20}]}> It is incredibly efficient in transporting information around. However, note that by utilizing this symmetric cryptography, the security around those keys must be airtight! If that single key were to be exposed to an attacker from either party, any encrypted data they acquire will be easily decrypted.</Text>
+                    <Text style={[styles.paragraph, { marginTop:20}]}> It is incredibly efficient in transporting information around. However, note that by utilizing this symmetric cryptography, the security around those keys must be airtight! If that single key were to be exposed to an attacker from either party, any encrypted data they acquire will be easily decrypted.</Text>
                 </View>
                 <View style={styles.sectionContent}>
                     <Text style ={[styles.title, { marginTop:20}]}>Public-key Encryption</Text>
@@ -122,7 +156,7 @@ export default function WebTracking(props) {
                     <Text style ={[styles.paragraph]}>Above we can see that both parties need their own pair of private-public keys. Alice's message is encrypted with Bob's public key so that the only way it can be decrypted is if the other party knew of Bob's private key.</Text>
                 </View>
                 <View style={styles.sectionContent}>
-                    <Text style = {[styles.paragraph]}>Public-key encryption is certainly secure what with the deciphering tool being more obfuscated. One problem that you might find when implementing it is that it will be much more power and time consuming. With two keys being assigned to every person in the network, computational power and storage requirements will be important to focus on.</Text>
+                    <Text style={[styles.paragraph]}>Public-key encryption is certainly secure what with the deciphering tool being more obfuscated. One problem that you might find when implementing it is that it will be much more power and time consuming. With two keys being assigned to every person in the network, computational power and storage requirements will be important to focus on.</Text>
                 </View>
             </View>)}
             <View style={[styles.sectionNav,{margin:20}]}>
